89af622b942cf3107b7bce6f37f71426efcac683
veygel <vleygel@gmail.com>
2021-10-11 13:17:35 +0300

    second commit

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 document.addEventListener("DOMContentLoaded", init);
 
+const selectElement = document.querySelector('.filter');
+selectElement.addEventListener('change', (event) => {
+    if (event.target.value === 'now_playing') {
+        getresults(event.target.value);
+        renderPage(mainHtmlStr);
+    } else if (event.target.value === 'popular') {
+        getresults(event.target.value);
+        renderPage(mainHtmlStr);
+    } else if (event.target.value === 'favorites') {
+        showFavorites()
+    }
+
+});
+
+var favorites = [];
+var movies = [];
+var mainHtmlStr = '';
+
 function init() {
     getresults();
 }
-const url = 'https://api.themoviedb.org/3/discover/movie?api_key=2c46288716a18fb7aadcc2a801f3fc6b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&';
 
-function getresults() {
+function getresults(filter = 'popular') {
+    const url = `https://api.themoviedb.org/3/movie/${filter}?api_key=2c46288716a18fb7aadcc2a801f3fc6b&language=en-US&page=10`;
     fetch(url, {
             headers: new Headers({
                 'Accept': 'application/json'
@@ -14,21 +32,60 @@ function getresults() {
         .then(res => { return res.text() })
         .then(res => {
             let data = JSON.parse(res);
-            var htmlStr = '';
-
+            // console.log(data)
+            movies = data.results;
+            let obj = {}
             for (let itr = 0; itr < data.results.length; itr++) {
-                htmlStr += `
-                <a href="https://www.themoviedb.org/movie/${data.results[itr]?.id}">
-                <div class="movieCard">
+                mainHtmlStr += `
+                <div class="movieCard" onclick="selectMovie(${data.results[itr]?.id})">
                 <h2>${data.results[itr]?.original_title}</h2>
-                <p>${data.results[itr]?.overview}</p>
                 <img src="https://image.tmdb.org/t/p/original${data.results[itr]?.poster_path}" alt="">
-            </div></a>`
+            </div>`
             }
-            console.log(htmlStr)
-            document.querySelector("#movieList").innerHTML =
-                htmlStr
+            renderPage(mainHtmlStr);
         }).catch(error => {
             throw (error);
         })
+}
+
+function selectMovie(id) {
+    let movie = movies.find(item => item.id === id)
+    let htmlStr = `
+    <div class="moviePage">
+    <div class="buttons flex space-between">
+    <button onclick="homePage()">back</button>
+    <button onclick="addToFavorites(${movie.id})">add to favorites</button>
+    </div>
+    <h2>${movie.original_title}</h2>
+    <p>${movie.overview}</p>
+    <div class="imageWrap">
+        <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="">
+    </div>
+</div>`;
+    renderPage(htmlStr)
+}
+
+function renderPage(str) {
+    document.querySelector("#content").innerHTML = str;
+}
+
+function addToFavorites(movieId) {
+    if (favorites.indexOf(movieId) !== -1) return;
+    favorites.push(movies.find(item => item.id === movieId))
+}
+
+function showFavorites() {
+    var htmlStr = ''
+    for (let itr = 0; itr < favorites.length; itr++) {
+        htmlStr += `
+            <div class="movieCard" onclick="selectMovie(${favorites[itr]?.id})">
+            <h2>${favorites[itr]?.original_title}</h2>
+            <img src="https://image.tmdb.org/t/p/original${favorites[itr]?.poster_path}" alt="">
+        </div>`
+    }
+    renderPage(htmlStr)
+}
+
+function homePage() {
+    renderPage(mainHtmlStr)
 }
